1a5d2de69ea83bda73a3edc1361d237129e125b3
w5x28 <w5x28@students.keele.ac.uk>
2021-03-12 11:17:18 +0000

    added attendees to controller

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -2,6 +2,7 @@ const db = require('../../config/db')
 require('dotenv').config()
 const GetLoginCount = async (req, res) => {
     try {
+      sql='select * from applogin'
       con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Result: " + result);
@@ -16,7 +17,23 @@ const GetLoginCount = async (req, res) => {
     }
   };
 
-
+  const GetAtendeeSession = async (req, res) => {
+    try {
+      sql='select * from applogin'
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + result);
+        return res.status(200).json(result);
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(400).json({
+        success: false,
+        data: err,
+      });
+    }
+  };
   module.exports = {
     GetLoginCount,
+    GetAtendeeSession,
   };
